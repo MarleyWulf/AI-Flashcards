1404b7ce4256c7abdb0bb020bcc52fb6e27ce748
Marley <pollardmarlon7@gmail.com>
2024-08-25 14:52:13 -0400

    Working on the clerk sign up/sign in and the main layout

---
+++ app/sign-up/page.js
@@ -0,0 +1,7 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Box, Typography } from "@mui/material";
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUp() {
+  return <SignUp />;
+}
---
+++ components/ToolBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, AppBar, Typography, Stack, Toolbar, Button } from "@mui/material";
+import Link from "next/link";
+import SignUp from "@/app/Sign-up/page";
+import Login from "@/app/Log-in/page";
+
+export default function ToolBar({ loginLink, signUpLink }) {
+  return (
+    <Stack>
+      <Box>
+        <AppBar position="static" sx={{ backgroundColor: "black" }}>
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <Typography>Flashcards AI</Typography>
+            <Box sx={{ ml: "auto" }}>
+              <Button>
+                <Link href={loginLink}>Login</Link>
+              </Button>
+              <Button>
+                <Link href={signUpLink}>Sign Up</Link>
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Stack>
+  );
+}
